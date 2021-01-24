e42dc7ee17af787f90f0047c98426e7fb406edf8
Naoki Ito <naoki.109.0525@gmail.com>
2021-01-24 12:49:36 +0900

    usersコントローラの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  has_secure_password
+
   validates :name,presence: true,length: {maximum: 30}
   validate :validate_name_not_including_conma
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_033311) do
+ActiveRecord::Schema.define(version: 2021_01_24_034447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_033311) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
